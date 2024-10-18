0476c493678e6c17597f98cf7280e6d0f1f35246
QuesilloLover <adiliamoreno11@gmail.com>
2024-10-18 01:57:41 -0600

    kak

--- backend/NiCode/users/serializers.py
+++ backend/NiCode/users/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import CustomUser
 
 class CustomUserSerializer(serializers.ModelSerializer):
+    # This is the serializer for the CustomUser model
     class Meta:
         model = CustomUser
         fields = ['id','username', 'email','first_name','last_name','bio', 'elo', 'profile_image', 'guild_id', 'role_guild_id']
